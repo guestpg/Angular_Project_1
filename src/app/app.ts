df540214f67cb16a8f2fe5553a49cbef3adb4d45
pyramidguest <pyramidguest@gmail.com>
2025-10-05 18:17:31 +0000

    applied wiritalble signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,10 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-import {Component} from '@angular/core';
+import {Component,signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {RecipeModel} from './models';
+import {MOCK_RECIPES} from './mock-recipes';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +16,21 @@ import {RouterOutlet} from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
+
+
 export class App {
-  protected title = 'yapp';
+  protected readonly recipe = signal<RecipeModel>(MOCK_RECIPES[0]);
+
+    
+  protected selectRecipe1(): void {
+    this.recipe.set(MOCK_RECIPES[0]);
+  }
+
+   protected selectRecipe2(): void {
+    this.recipe.set(MOCK_RECIPES[1]);
+  }
+
 }
+
+
+
